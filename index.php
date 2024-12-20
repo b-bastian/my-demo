8747151d3b874e91f72b314384da261aee622294
b-bastian <bbrugger@schueler.hakzell.at>
2024-12-20 09:01:20 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use BBastian\MyDemo\santa;
+
+require_once "vendor/autoload.php";
+
+$santa = new santa();
+
+$santa->HoHoHo();
---
+++ src/santa.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BBastian\MyDemo;
+
+class santa {
+    public function HoHoHo() {
+        echo "Ho Ho Ho!";
+    }
+}
